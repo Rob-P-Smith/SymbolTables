e88d7a767568990b2947a95d41419dc4d459bb51
Clav <smith.rob@student.greenriver.edu>
2024-02-08 13:24:32 -0800

    Fixing up for class

--- src/SequentialSearchST.java
+++ src/SequentialSearchST.java
@@ -1,117 +1,66 @@
-public class SequentialSearchST<Key, Value> implements SymbolTable<Key, Value>{
+public class SequentialSearchST<Key, Value> implements SymbolTable<Key, Value> {
     private Node head;
     private int size;
+
     private class Node {
         Key key;
         Value value;
         Node next;
 
-        public Node(Key key, Value value, Node next){
+        //constructor
+        public Node(Key key, Value value, Node next) {
             this.key = key;
             this.value = value;
             this.next = next;
         }
+
     }
 
-    public SequentialSearchST(){
+    public SequentialSearchST() {
         head = null;
         size = 0;
     }
 
-    /**
-     * Put a key-value pair into the table
-     *
-     * @param key
-     * @param val
-     */
     @Override
     public void put(Key key, Value val) {
-        Node current = head;
-        while(current!=null){
-            if (key.equals(current.key)){
-                current.value = val;
-                return;
-            }
-            current=current.next;
-        }
-        Node newNode = new Node(key, val, head.next);
-        head.next=newNode;
-    }
+        // if the key is not in the list, put it up front at the head
+        // but if key is already in the list, replace the value
+        //  in that node
 
-    /**
-     * Returns the value paired with the given key.
-     *
-     * @param key
-     */
-    @Override
-    public Value get(Key key) {
+        // loop through the nodes to see if the key is there
         Node current = head;
-        while(current!=null){
-            if(key.equals(current.key)){
-                return current.value;
+        while (current != null) {
+            if (key.equals(current.key)) {
+                // we found the key! So replace the value
+                current.value = val;
+                return; // done!
             }
             current = current.next;
         }
+
+        // if we got here, we know key doesn't exist in list
+        // so make a new node and put it at the front
+        head = new Node(key, val, head);
+        size++;
+    }
+
+    @Override
+    public Value get(Key key) {
+        for (Node current = head; current != null; current = current.next) {
+            if (key.equals(current.key)) {
+                return current.value;
+            }
+        }
+
+        // if I got here, I know key is not in the list
         return null;
     }
 
-    /**
-     * Remove key (and it's value) from the table.
-     *
-     * @param key
-     */
-    @Override
-    public void delete(Key key) {
-        Node current = head.next;
-        Node previous = head;
-        while(current!=null){
-            if(key.equals(current.key)){
-                previous.next = current.next;
-            }
-            previous = current;
-            current = current.next;
-        }
-    }
-
-    /**
-     * Returns true if there is a value paired with a key.
-     *
-     * @param key
-     */
-    @Override
-    public boolean contains(Key key) {
-        Node current = head;
-        while(current!=null) {
-            if (current.key.equals(key)) return true;
-            current=current.next;
-        }
-        return false;
-    }
-
-    /**
-     * Returns true if the table is empty.
-     *
-     * @return
-     */
-    @Override
-    public boolean isEmpty() {
-        return SymbolTable.super.isEmpty();
-    }
-
-    /**
-     * Returns the number of key-value pairs in the table.
-     *
-     * @return
-     */
     @Override
     public int size() {
         return size;
     }
 
-    /**
-     * Returns an iterator that refers to all the keys
-     * in the table.
-     */
     @Override
     public Iterable<Key> keys() {
         return null;
